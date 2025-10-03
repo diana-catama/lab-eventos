23e471e53143d98aa6e5daa4c9e47ba505ad282d
carolina <dianacarolinacatama@gmail.com>
2025-10-02 21:27:11 -0500

    localstorage

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const form = document.getElementById("postForm");
 const feed = document.getElementById("feed");
 
+window.addEventListener("DOMContentLoaded", () => {
+  let publicaciones = JSON.parse(localStorage.getItem("publicaciones")) || [];
+  publicaciones.forEach(pub => mostrarPublicacion(pub.titulo, pub.descripcion, pub.imagen));
+})
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -10,16 +14,42 @@ form.addEventListener("submit", (e) => {
   const imagenInput = document.getElementById("imagen");
   const archivo = imagenInput.files[0];
 
+  let publicacion = JSON.parse(localStorage.getItem("publicaciones")) || [];
+
   if (!archivo) return;
 
-  const imgURL = URL.createObjectURL(archivo);
+  const archivoR = new FileReader();
+  archivoR.onload = function(e){
+    const img64 = e.target.result;
 
-  const col = document.createElement("div");
+    let publicaciones = JSON.parse(localStorage.getItem("publicaciones")) || [];
+
+    const nuevaPublicacion = {
+      titulo,
+      descripcion,
+      imagen: img64
+    };
+
+    publicaciones.unshift(nuevaPublicacion);
+
+    localStorage.setItem("publicaciones", JSON.stringify(publicaciones));
+
+    mostrarPublicacion(titulo, descripcion, img64)
+  };
+
+  archivoR.readAsDataURL(archivo);
+
+  form.reset();
+
+});
+
+function mostrarPublicacion(titulo, descripcion, imagen) {
+    const col = document.createElement("div");
   col.classList.add("col-md-6", "col-lg-4");
 
   col.innerHTML = `
-    <div class="card shadow-sm h-100">
-      <img src="${imgURL}" alt="Publicación">
+    <div class="card shadow-sm h-100 publicacion">
+      <img src="${imagen}" alt="Publicación">
       <div class="card-body d-flex flex-column">
         <h5 class="card-title">${titulo}</h5>
         <p class="card-text">${descripcion}</p>
@@ -27,8 +57,6 @@ form.addEventListener("submit", (e) => {
       </div>
     </div>
   `;
-
-  feed.prepend(col);
 
   const likeBtn = col.querySelector(".like-btn");
   const likeCount = col.querySelector(".like-count");
@@ -44,6 +72,5 @@ form.addEventListener("submit", (e) => {
       likeCount.textContent = parseInt(likeCount.textContent) - 1;
     }
   });
-
-  form.reset();
-});
+  feed.prepend(col);
+};
